05ae4db39df2089da58b12dba5c3173726ad361c
rafael costa <rafael.costa@made2web.com>
2022-12-21 15:35:42 +0000

    Texto alterado

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ export default function Home({ allPostsData }) {
           </Link>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <Link href="https://nextjs.org/learn">our Next.js tutorial</Link>.)
+          (Tutorial disponível em:{" "}
+          <Link href="https://nextjs.org/learn">Next.js tutorial</Link>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
